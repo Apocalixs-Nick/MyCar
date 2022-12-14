1cf60627b07d2b88062412c8ce3190fba6cf790b
Apocalixs-Nick <nicolapiccirillo04@gmail.com>
2022-12-14 16:05:11 +0100

    Creation of test classes

---
+++ app/src/androidTest/java/com/example/mycar/CarListFragmentInstrumentedTest.kt
@@ -0,0 +1,12 @@
+package com.example.mycar
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CarListFragmentInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+}
