d547ef1635c7a3674ceb7605bd6b4123fdfb8ca5
Apocalixs-Nick <nicolapiccirillo04@gmail.com>
2022-12-16 11:43:35 +0100

    Adding first version of the functions to be reused for the test

--- app/src/androidTest/java/com/example/mycar/NavigationInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/mycar/NavigationInstrumentedTest.kt
@@ -2,6 +2,9 @@ package com.example.mycar
 
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -16,4 +19,20 @@ class NavigationInstrumentedTest {
     val navController = TestNavHostController(
         ApplicationProvider.getApplicationContext()
     )
+
+    // TODO: To be completed and reviewed if it is the right approach
+    @Test
+    fun navigateTest() {
+        val navigateItems = setOf (
+            R.id.action_carListFragment_to_addCarFragment,
+            R.id.action_addCarFragment_to_carListFragment,
+            R.id.action_carListFragment_to_carDetailFragment,
+            R.id.action_carDetailFragment_to_addCarFragment
+        )
+
+        navigateItems.forEach {
+            onView(withId(it))
+                .perform(click())
+        }
+    }
 }
--- app/src/androidTest/java/com/example/mycar/TestFunctions.kt
+++ app/src/androidTest/java/com/example/mycar/TestFunctions.kt
@@ -15,3 +15,109 @@ fun goToHomeFromAddFragment() {
     onView(withId(R.id.action_addCarFragment_to_carListFragment))
         .perform(click())
 }
+
+fun goToDetailFromHome() {
+    onView(withId(R.id.action_carListFragment_to_carDetailFragment))
+        .perform(click())
+}
+
+fun goToAddFromDetailFragment() {
+    onView(withId(R.id.action_carDetailFragment_to_addCarFragment))
+        .perform(click())
+}
+
+fun goToHome() {
+    onView(withId(R.id.carListFragment))
+        .perform(click())
+}
+
+fun goToAdd() {
+    onView(withId(R.id.addCarFragment))
+        .perform(click())
+}
+
+fun goToDetail() {
+    onView(withId(R.id.carDetailFragment))
+        .perform(click())
+}
+
+fun clickAddNewCar() {
+    onView(withId(R.id.add_car))
+        .perform(click())
+}
+
+fun clickFloatingDetail() {
+    onView(withId(R.id.detail_car))
+        .perform(click())
+}
+
+fun clickEditFloatingDetail() {
+    onView(withId(R.id.edit_car))
+        .perform(click())
+}
+
+fun clickShareFloatingDetail() {
+    onView(withId(R.id.share_car))
+        .perform(click())
+}
+
+fun clickDeleteFloatingDetail() {
+    onView(withId(R.id.delete_car))
+        .perform(click())
+}
+
+fun clickId(idInput: Int){
+    onView(withId(idInput))
+        .perform(click())
+}
+
+//TODO: Review this function
+fun navigateToListToAdd() {
+    goToHome()
+    clickAddNewCar()
+    goToAdd()
+}
+
+fun navigateToListToDetail() {
+    goToHome()
+    goToDetail()
+    clickFloatingDetail()
+}
+
+fun navigateToListToDetailEdit() {
+    goToHome()
+    goToDetail()
+    clickFloatingDetail()
+    clickEditFloatingDetail()
+}
+
+fun navigateToListToDetailShare() {
+    goToHome()
+    goToDetail()
+    clickFloatingDetail()
+    clickShareFloatingDetail()
+}
+
+fun navigateToListToDetailDelete() {
+    goToHome()
+    goToDetail()
+    clickFloatingDetail()
+    clickDeleteFloatingDetail()
+}
+
+fun addNewCar() {
+    navigateToListToAdd()
+
+}
+
+fun updateCar() {
+    navigateToListToDetailEdit()
+}
+
+fun shareCar() {
+    navigateToListToDetailShare()
+}
+
+fun deleteCar() {
+    navigateToListToDetailDelete()
+}
