ca3d41f9bacdf695468d614ca7944bba1f1c58d9
Apocalixs-Nick <nicolapiccirillo04@gmail.com>
2023-01-12 09:59:27 +0100

    Added alert dialog for door and seat textfield with enum class DefaultDoor and DefaultSeat

---
+++ app/src/main/java/com/example/mycar/utils/enum/DefaultDoor.kt
@@ -0,0 +1,6 @@
+package com.example.mycar.utils.enum
+
+enum class DefaultDoor (val door: String) {
+    D_3("3"),
+    D_5("5")
+}
---
+++ app/src/main/java/com/example/mycar/utils/enum/DefaultSeat.kt
@@ -0,0 +1,12 @@
+package com.example.mycar.utils.enum
+
+enum class DefaultSeat (val seat: String) {
+    S_2("2"),
+    S_3("3"),
+    S_4("4"),
+    S_5("5"),
+    S_6("6"),
+    S_7("7"),
+    S_8("8"),
+    S_9("9")
+}
